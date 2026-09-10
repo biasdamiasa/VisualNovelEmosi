295e602e5e4e9975e6243ee7c4db7ca0f7d81b5a
biasdamiasa <38208639+biasdamiasa@users.noreply.github.com>
2026-09-10 11:30:27 +0700

    initial commit

---
+++ Assets/Script/CharacterVisualScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Yarn.Unity;
+
+public class CharacterVisualScript : MonoBehaviour
+{
+    public SpriteRenderer reyDummy;
+
+    [YarnCommand("show_tense")]
+    public void ShowTense()
+    {
+        reyDummy.color = Color.yellow; 
+    }
+
+    [YarnCommand("show_normal")]
+    public void ShowNormal()
+    {
+        reyDummy.color = Color.white;
+    }
+}
